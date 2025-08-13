d832c4748714f39e211ccc89e979c1b32346be05
piyushGit1229 <varmapiyush864@gmail.com>
2025-08-13 22:01:36 +0530

    Initial commit

---
+++ Leetcode Questions/Strings/125. Valid Palindrome.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool ispalindrome(string s){
+    int i=0;
+    int j=0;
+    while(i<=j){
+        while(i<j && !isalnum(s[i])){
+            i++;
+        }
+        while(i<j && !isalnum(s[j])){
+            j--;
+        }
+        if(tolower(s[i])!=tolower(s[j])){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+int main(){
+    string s;
+    cout<<"Enter the string: ";
+    cin>>s;
+    if (ispalindrome(s)) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is NOT a palindrome." << endl;
+    }
+
+return 0;
+}
