78411d08473f61a54c5aa6f31a43bb72e5bf1fd9
Piyush varma <varmapiyush864@gmail.com>
2025-09-12 17:21:54 +0530

    Solved

--- Leetcode Questions/LinkedList/21. Merge Two Sorted Lists.cpp
+++ Leetcode Questions/LinkedList/21. Merge Two Sorted Lists.cpp
@@ -17,3 +17,28 @@
 
 //     }
 // };
+
+
+ // Write your code here.
+    //we are given list 1 and list 2 
+    //we will start with iterative approch
+
+    // Node<int>* dummy = new Node(0);
+    // Node<int>* tail = dummy;
+
+    // while(first && second){
+    //     if(first->data <= second->data){
+    //         tail->next = first;
+    //         first=first->next;
+    //     }
+    //     else{
+    //         tail->next = second;
+    //         second = second->next;
+    //     }
+    //     tail = tail->next;
+    // }
+    // if(first) tail ->next= first;
+    // if(second) tail ->next = second;
+
+    // return dummy->next;
+    
