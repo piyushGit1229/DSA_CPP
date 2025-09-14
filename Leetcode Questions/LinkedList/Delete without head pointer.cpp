0e8a2475bd35193d1a9fdeb9a5e26b699c633310
Piyush varma <varmapiyush864@gmail.com>
2025-09-14 15:44:03 +0530

    solved

---
+++ Leetcode Questions/LinkedList/Delete without head pointer.cpp
@@ -0,0 +1,24 @@
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}*head;
+*/
+
+// class Solution {
+//   public:
+//     void deleteNode(Node* del_node) {
+//         // code here
+//     //replicate and copy the vaklue of the node next to del_node
+//     Node* temp = del_node->next;
+//     del_node->data=temp->data;
+//     del_node -> next = del_node->next->next;
+    
+//     free(temp);
+        
+//     }
+// };
