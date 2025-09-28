dd107b466d73bc43bd803a95d30b7d2a80891de3
Piyush varma <varmapiyush864@gmail.com>
2025-09-29 00:24:31 +0530

    solved

---
+++ Leetcode Questions/STACKS/946. Validate Stack Sequences.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool validateStackSequences(vector<int> &pushed, vector<int> &popped)
+    {
+        stack<int> st;
+        int j = 0;
+        for (int x : pushed)
+        {
+            st.push(x);
+
+            while (!st.empty() && j < popped.size() && st.top() == popped[j])
+            {
+                st.pop();
+                j++;
+            }
+        }
+        return st.empty();
+    }
+};
+int main()
+{
+
+    return 0;
+}
